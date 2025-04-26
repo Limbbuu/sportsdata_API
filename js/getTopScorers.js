e5834d8b1a99c6f93ee12f8fbe9d5043fd3aa17a
Limbbuu <linus.nurminen@gmail.com>
2025-04-26 21:26:19 +0300

    lisätty tiedosto joka tarjoaa maalintekijätilastoja valitusta sarjata ja kaudesta

---
+++ js/getTopScorers.js
@@ -0,0 +1,47 @@
+import { API_KEY } from './config.js';
+
+//export asyncissä = voidaan käyttää myös muualla
+export async function fetchTopScorers(leagueId, season) {
+  try {
+    const response = await fetch(`https://v3.football.api-sports.io/players/topscorers?league=${leagueId}&season=${season}`, {
+      method: 'GET',
+      headers: {
+        'x-apisports-key': API_KEY
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Virhe maalintekijöiden haussa: ${response.status}`);
+    }
+
+    const data = await response.json();
+    displayTopScorers(data.response);
+  } catch (error) {
+    console.error('Virhe haettaessa maalintekijöitä:', error);
+  }
+}
+
+function displayTopScorers(players) {
+  const scorersElement = document.getElementById('topscorers-table');
+  scorersElement.innerHTML = `
+    <tr>
+      <th></th>
+      <th>nimi</th>
+      <th>maalit</th>
+    </tr>   
+    `;
+
+  players.forEach(playerData => {
+    const player = playerData.player;
+    const stats = playerData.statistics[0];
+
+
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td><img src="${player.photo}" width="30" style="vertical-align:middle; margin-right: 8px;"></td>
+    <td>${player.name}</td>
+    <td>${stats.goals.total} </td>
+    `;
+    scorersElement.appendChild(row);
+  });
+}
